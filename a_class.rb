5ebac3a24c5bd1bba5516732d0d19e920d7f84ac
Ryan Angilly <ryan@angilly.com>
2010-05-03 15:59:08 -0400

    It's raining a lot here.  Here are some sample programs

---
+++ a_class.rb
@@ -0,0 +1,9 @@
+# This is a simple program in a class.
+
+class AClass
+  def run
+    puts "Oh hi"
+  end
+end
+
+AClass.new.run
---
+++ stand_alone.rb
@@ -0,0 +1,4 @@
+# This is a comment.  Not executed.
+# Run this guy by typing "ruby ./stand_alone.rb"
+
+puts "This is a program all by itself"
